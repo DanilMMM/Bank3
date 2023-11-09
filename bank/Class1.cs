b34ba79f93bedae855ca4e5488679c1b3a28d2b7
DanilMMM <115102963+DanilMMM@users.noreply.github.com>
2023-11-09 13:20:06 +0300

    Add files via upload

---
+++ bank/Class1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+class Bank
+{
+
+    
+    public int Ncheta;
+    public string FIO;
+    public float sum;
+    public int vibor;
+
+    
+
+    public  Bank (string FIO, int Ncheta, float sum)
+    {
+        this.FIO = FIO;
+        this.Ncheta = Ncheta; 
+        this.sum = sum;
+    }  
+    public void Out()           
+    {
+        Console.WriteLine($"Номер счета {Ncheta} \nВладелец счета {FIO}  \nКол-во средств на счету {sum}");
+           
+    }
+                                                                                                                                                                       
+    public void Dob(float sum)
+    {
+        float perv = this.sum;                          
+        Console.WriteLine($"Кол-во денег на счету {perv} \nПополнить счет на {sum} ?");
+        sum = perv + sum;
+        Console.WriteLine($"Ваш баланс {sum}");
+    } 
+    public void umen (float sum)
+    {
+        float perv = this.sum;
+        Console.WriteLine($"Кол-во денег на счету {perv} \nСнять со счета {sum} ?");
+        sum = perv - sum;
+        Console.WriteLine($"Ваш баланс {sum}");
+    }
+
+    public void obnul(float sum)                                                                                                                                                
+    {
+        sum = sum;
+        Console.WriteLine($"Снять со счета все средства ?");
+        sum = sum - sum;
+        Console.WriteLine($"Баланс {sum}");
+    }
+
+   // public void Perevod (float per)
+  //  {
+  //////////////////////////////////////  ////////////////////////////////////     
+     //   Console.WriteLine($"Введите сумму которую хотите перевести ");
+       // Console.ReadLine();
+       // sum = sum - per;
+        //Console.WriteLine($"Ваш баланс {sum} ");
+
+   // }
+
+    public void Perevod (Bank bank)
+    {
+        Console.WriteLine($"Сколько вы хотите перевести руб. на счёт {bank.Ncheta}");
+        float Amount = Convert.ToSingle(Console.ReadLine());
+
+        if (sum >= Amount)
+        {
+            bank.sum += Amount;
+            sum -= Amount;
+            Console.WriteLine($"Перевод на счёт {bank.Ncheta} выполнен успешно");
+        }
+        else
+        {
+            Console.WriteLine("Недостаточно средств для перевода");
+        }
+    }
+
+
+    public void Syst(Bank bank)
+    {
+        int proverka = 0;
+        do
+        {
+            Console.WriteLine("Нажмите 0 если хотите выйти из приложения банка\nНажмите 1 если хотите пополнить счёт.\nНажмите 2 если хотите снять деньги со счёта\nНажмите 3 если хотите снять все деньги со счёта\nНажмите 4 если хотите перевести деньги на другой счёт");
+            string vibor = (Console.ReadLine());
+            switch (vibor)
+            {
+                case "1" :
+                    Dob(9965906);
+                    break;
+                case "2" :
+                    umen(9876);
+                    break;
+                case "3" :
+                    obnul(2345);
+                    break;
+                case "4" :
+                    Perevod(bank);
+                    break;
+                case "0" :
+                    proverka++;
+                    break;
+                default:
+                    Console.WriteLine("Ни одна операция не выполнена");
+                    break;
+            }
+        }
+        while (proverka == 0);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
---
+++ bank/Program.cs
@@ -0,0 +1,23 @@
+﻿    
+   
+    
+
+
+Bank user1 = new Bank ("R", 123456789, 320);
+Bank user2 = new Bank ("U", 987654321, 500);
+Bank user3 = new Bank ("S", 123123123, 800);
+
+
+
+
+user3.Out();
+user3.Syst(user1);
+user1.Out();
+//user1.Out();
+//user2.Out();
+//user1.Dob();
+//user1.Umen();
+//user1.Transfer(user2);
+//user1.Obnul();
+//user2.Obnul();
+//user1.Zachem();
